7c3391d0c6b5b9d511097ca89c21c75125392f7f
Tijmen van der Spijk <tijmenspijk@gmail.com>
2023-06-29 04:34:47 +0200

    trying to configure picom

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -6,6 +6,9 @@
 	];
 
 	home.file.".config/bspwm/bspwmrc".source = ../../.config/bspwmrc;
-    	home.file.".config/sxhkd/sxhkdrc".source = ../../.config/sxhkdrc;
+	home.file.".config/sxhkd/sxhkdrc".source = ../../.config/sxhkdrc;
+
 	home.file.".config/fish/config.fish".source = ../../.config/config.fish;
+
+	home.file.".config/picom/picom.conf".source = ../../.config/picom.conf;
 }
