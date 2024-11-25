8e4e9085e843eccb09939440de1e1df953f82a33
Tijmen van der Spijk <tijmenspijk@gmail.com>
2024-11-25 18:41:08 +0100

    fixing stuff

--- home.nix
+++ home.nix
@@ -3,7 +3,17 @@
 {
 	home.username = "tijmen";
 	home.homeDirectory = "/home/tijmen";
+	
+	programs.vscode = {
+  		enable = true;
+	};
 
+  	programs.git = {
+    		enable = true;
+    		userName  = "Tijmen van der Spijk";
+    		userEmail = "tijmenspijk@gmail.com";
+  	};	
+	
 	home.stateVersion = "25.05";
 	programs.home-manager.enable = true;
 }
