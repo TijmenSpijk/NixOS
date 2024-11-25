9f49657328f9ac3b3c59584d3efa1b34ca0b734f
Tijmen van der Spijk <tijmenspijk@gmail.com>
2024-11-25 20:09:22 +0100

    fixing modules

--- flake.nix
+++ flake.nix
@@ -1,7 +1,10 @@
 {
   inputs = {
+    # Cosmic
     nixpkgs.follows = "nixos-cosmic/nixpkgs"; # NOTE: change "nixpkgs" to "nixpkgs-stable" to use stable NixOS release
     nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
+    
+    # Home Manager
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -9,7 +12,7 @@
   };
 
   outputs =
-    {
+    inputs@{
       self,
       nixpkgs,
       nixos-cosmic,
@@ -20,13 +23,18 @@
       nixosConfigurations = {
         nixos = nixpkgs.lib.nixosSystem {
           modules = [
+            ./configuration.nix
+
+            # Cosmic
             {
               nix.settings = {
                 substituters = [ "https://cosmic.cachix.org/" ];
                 trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
               };
-            }
+            }            
             nixos-cosmic.nixosModules.default
+
+            # Home Manager
             home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
@@ -34,7 +42,7 @@
               home-manager.users.tijmen = import ./home;
               home-manager.extraSpecialArgs = inputs;
             }
-            ./configuration.nix
+            
           ];
         };
       };
