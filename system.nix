2bd07ac4854a6be904675a27ec126c1402989b75
Tijmen van der Spijk <tijmenspijk@gmail.com>
2024-11-25 21:43:20 +0100

    trying to fix stylix

--- system.nix
+++ system.nix
@@ -48,7 +48,7 @@
 
   stylix = {
     enable = true;
-    image = "/home/tijmen/Pictures/wallpaper.jpg";
+    image = ./wallpaper.jpg;
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
   };
